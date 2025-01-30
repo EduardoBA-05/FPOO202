7c488b6d18e79cf0180fb63be244cd2559b3ab20
EduardoBA-05 <123046652@upq.edu.mx>
2025-01-30 14:38:28 -0600

    Práctica 4-Ejercicio 2-Completado

--- Practica4/src/Ejercicio2.java
+++ Practica4/src/Ejercicio2.java
@@ -5,7 +5,14 @@ public class Ejercicio2 {
         Scanner scn = new Scanner(System.in);
         int numero;
 
+        System.out.println("Ingresa un numero: ");
+        numero = scn.nextInt();
 
+       if(numero%2==0){
+           System.out.println("El numero es par");
+       } else {
+           System.out.println(("El numero es impar"));
+       }
 
     }
 }
