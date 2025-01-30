004c21efb4b8b337a57677bd752361cae4ebabed
EduardoBA-05 <123046652@upq.edu.mx>
2025-01-30 14:47:44 -0600

    Práctica 4-Ejercicio3-Completado

--- Practica4/src/Ejercicio3.java
+++ Practica4/src/Ejercicio3.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
     public static void main(String[] args) {
-        
+        int edad;
+        double precio;
+        Scanner scn = new Scanner(System.in);
+
+
+        System.out.println("Ingresa tu edad");
+        edad = scn.nextInt();
+
+        if(edad<=4){
+            System.out.println("Puedes entrar gratis");
+            precio = 0;
+
+        }else if (edad<=18) {
+            System.out.println("La entrada cuesta 110");
+            precio = 110;
+        } else if (edad>18) {
+            System.out.println("La entrada cuesta 190");
+            precio = 190;
+        }
     }
 }
