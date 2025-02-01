4f5d42164b51b747b361142fb2d68cebf200dc18
EduardoBA-05 <123046652@upq.edu.mx>
2025-02-01 01:32:44 -0600

    Práctica 5- Ejercicio 7- Completado

---
+++ Practica5/src/Ejercicio7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int i = 0;
+        int base;
+        int filas;
+
+        System.out.print("Ingresa el numero para la base del arbol: ");
+        base = scn.nextInt();
+
+        scn.close();
+
+        filas = (base +1) / 2;
+
+        while (i< filas) {
+            System.out.println(" ".repeat(filas -i - 1)+ "*".repeat(2* i + 1));
+            i++;
+
+
+
+
+
+        }
+
+
+    }
+
+}
