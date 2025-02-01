9463e5c7c7a97c1e66f150f28e1ba2225c7770e1
EduardoBA-05 <123046652@upq.edu.mx>
2025-01-31 19:57:18 -0600

    Práctica 5 - Ejercicio 2 - Completado

---
+++ Practica5/src/Ejercicio2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int numero;
+
+        System.out.println("Introduce un numero: ");
+        numero = scn.nextInt();
+
+        if (numero>0){
+            for (int i= numero ; i<=numero; i--){
+
+
+                System.out.print(i);
+                System.out.print(",");
+
+                if (i==0){
+                    break;
+                }
+
+            }
+
+
+        } else {
+            System.out.println("El programa solo funciona con numeros positivos");
+        }
+
+
+
+
+
+
+    }
+}
